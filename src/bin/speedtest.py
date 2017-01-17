1a0162220cec7e9b882b522afac573b3835ea994
lmurphey <lmurphey@localhost>
2017-01-17 01:30:13 -0600

    Adding ability to define options for the speedtest command

--- src/bin/speedtest.py
+++ src/bin/speedtest.py
@@ -1,14 +1,20 @@
 from network_tools_app.search_command import SearchCommand
 from network_tools_app import pyspeedtest
+import sys
 
 class Speedtest(SearchCommand):
     
-    def __init__(self, runs=1):
+    def __init__(self, runs=1, server=None):
         SearchCommand.__init__(self, run_in_preview=True, logger_name="speedtest")
         
-        self.params = {
-            'runs' : runs
-        }
+        try:
+            self.params = {
+                'runs' : int(runs),
+                'host' : server
+            }
+        except ValueError:
+            sys.stderr.write('The runs parameter must be an integer')
+            return
         
         self.logger.info("Speedtest running")
     
@@ -20,9 +26,13 @@ class Speedtest(SearchCommand):
         result = {}
         
         st = pyspeedtest.SpeedTest(**self.params)
-        result['ping'] = st.ping()
-        result['download'] = st.download()
-        result['upload'] = st.upload()
+        result['ping'] = round(st.ping(),2)
+        result['download'] = round(st.download(),2)
+        result['download_readable'] = pyspeedtest.pretty_speed(st.download())
+        
+        result['upload'] = round(st.upload(),2)
+        result['upload_readable'] = pyspeedtest.pretty_speed(st.upload())
+        
         result['server'] = st.host
         
         # Output the results
