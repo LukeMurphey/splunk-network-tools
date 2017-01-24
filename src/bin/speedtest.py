0bd9df5d29a03d8f449ff03451d5b39acc95ffd2
lmurphey <lmurphey@localhost>
2017-01-24 00:31:07 -0600

    Changing "host" argument name to "server"
    Updating the name of the speedtest search command argument from host to
    server

--- src/bin/speedtest.py
+++ src/bin/speedtest.py
@@ -3,10 +3,10 @@ from network_tools_app.speedtest_helper import do_speedtest
 
 class Speedtest(SearchCommand):
     
-    def __init__(self, runs=1, host=None):
+    def __init__(self, runs=1, server=None):
         SearchCommand.__init__(self, run_in_preview=True, logger_name="speedtest_search_command")
         
-        self.host = host
+        self.server = server
         
         try:
             self.runs = int(runs)
@@ -20,7 +20,7 @@ class Speedtest(SearchCommand):
         # FYI: we ignore results since this is a generating command
         
         # Do the speedtest
-        result = do_speedtest(host=self.host, runs=self.runs, index="main", logger=self.logger)
+        result = do_speedtest(host=self.server, runs=self.runs, index="main", logger=self.logger)
         
         # Output the results
         self.output_results([result])
