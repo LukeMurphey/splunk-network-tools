d0e8e32fcd44fbb06e26f0f577a5ff440e055a35
LukeMurphey <Luke@LukeMurphey.net>
2017-03-10 14:25:57 -0600

    Adding parameter support
    Can now use customize test execution with arguments
    
    Reference #1778

--- tests/ui.py
+++ tests/ui.py
@@ -18,8 +18,53 @@ import importlib
 import sys
 import unittest
 import pkgutil
+import argparse
 
 from selenium.webdriver.common.by import By
+
+def setup_arg_parser():
+    """
+    Create an argument parser for handling CLI parameters.
+    """
+
+    parser = argparse.ArgumentParser(description='Run WebDriver UI tests exported by ' \
+    + 'SeleniumIDE in a way that works with Splunk.')
+
+    # Add the test case argument
+    parser.add_argument('--testcase', help='the test case to run', default=None)
+
+    # Add the Splunk Web URL argument
+    parser.add_argument('--url', help='the URL of SplunkWeb', default='http://127.0.0.1:8000')
+
+    # Add the Splunk username argument
+    parser.add_argument('--username', help='the user account to use when authenticating to Splunk',
+                        default='admin')
+
+    # Add the Splunk password argument
+    parser.add_argument('--password', help='the password to use when authenticating to Splunk',
+                        default='changeme')
+
+    return parser
+
+def remove_trailing_slash(url):
+    """
+    Remove the trailing slash from the URL if necessary.
+    """
+
+    if url[-1] == '/':
+        return url[:-1]
+    else:
+        return url
+
+# Parse the CLI arguments
+argument_parser = setup_arg_parser()
+arguments = argument_parser.parse_args()
+
+# Get the necessary parameters
+splunk_web_url = remove_trailing_slash(arguments.url)
+splunk_username = arguments.username
+splunk_password =  arguments.password
+test_case =  arguments.testcase
 
 def add_browser_driver_to_path():
     """
@@ -45,12 +90,18 @@ def load_all_modules_from_dir(dirname):
     """
     This function loads test cases from the test-case directory.
     """
+    cases_loaded = 0
 
     for (module_loader, name, ispkg) in pkgutil.iter_modules([dirname]):
         #importlib.import_module('ui_test_cases.' + name)
 
         #importlib.import_module('.' + name, __package__)
-        importlib.import_module('.' + name, 'ui_test_cases')
+        if test_case is None or name == test_case:
+            importlib.import_module('.' + name, 'ui_test_cases')
+            cases_loaded += 1
+
+    if cases_loaded == 0:
+        raise Exception("No test cases were loaded")
 
 def do_login(self):
     """
@@ -58,11 +109,11 @@ def do_login(self):
     """
 
     driver = self.driver
-    driver.get("http://127.0.0.1:8000/en-US/account/login")
+    driver.get(splunk_web_url + "/en-US/account/login")
     driver.find_element_by_id("username").clear()
-    driver.find_element_by_id("username").send_keys("admin")
+    driver.find_element_by_id("username").send_keys(splunk_username)
     driver.find_element_by_id("password").clear()
-    driver.find_element_by_id("password").send_keys("changeme")
+    driver.find_element_by_id("password").send_keys(splunk_password)
     driver.find_element_by_css_selector("input.splButton-primary.btn").click()
     self.assertTrue(self.is_element_present(By.CSS_SELECTOR, "a.manage-apps"))
 
@@ -102,7 +153,10 @@ load_all_modules_from_dir('ui_test_cases')
 # Make a test suite
 suite = unittest.TestSuite()
 
-for test_case_class in unittest.TestCase.__subclasses__()[1:]:
+for test_case_class in unittest.TestCase.__subclasses__()[1:]: # TODO: better handle this
+
     suite.addTest(unittest.makeSuite(patch_test_case(test_case_class)))
 
 result = unittest.TextTestRunner(verbosity=2).run(suite)
+sys.exit(not result.wasSuccessful())
+print result
