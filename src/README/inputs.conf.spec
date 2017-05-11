2ec5704f7b1d8b8513b9503a3298fd2f300dddbf
LukeMurphey <Luke@LukeMurphey.net>
2017-05-11 02:22:31 -0500

    Adding ability to do speedtest via a modular input
    Reference #1731

---
+++ src/README/inputs.conf.spec
@@ -0,0 +1,10 @@
+
+[speedtest://default]
+* Configure an input to do a Internet connection performance test
+
+server = <value>
+* The server to use when testing
+* A server will be automatically selected if this is left blank
+
+runs = <value>
+* The number of runs to execute
--- src/bin/speedtest.py
+++ src/bin/speedtest.py
@@ -1,34 +1,55 @@
-from network_tools_app.search_command import SearchCommand
+
+import sys
+
+from network_tools_app.modular_input import Field, ModularInput, IntegerField
+
 from network_tools_app import speedtest, get_default_index
 
-class Speedtest(SearchCommand):
-    """
-    A Search command for performing a speedtest.
-    """
+class SpeedtestInput(ModularInput):
+    def __init__(self, timeout=30):
 
-    def __init__(self, runs=1, server=None):
-        SearchCommand.__init__(self, run_in_preview=False, logger_name="speedtest_search_command")
+        scheme_args = {'title': "Internet Connection Speedtest",
+                       'description': "A speedtest of the Internet connection",
+                       'use_single_instance': False}
 
-        self.server = server
+        args = [
+            Field("server", "Server", "The server to use for testing; will be automatically assigned if left blank", empty_allowed=True, none_allowed=True, required_on_create=False, required_on_edit=False),
+            IntegerField("runs", "Runs", "The number of runs that should be executed", empty_allowed=False, none_allowed=False)
+        ]
 
-        try:
-            self.runs = int(runs)
-        except ValueError:
-            raise ValueError('The runs parameter must be an integer')
+        ModularInput.__init__(self, scheme_args, args, logger_name='speedtest_modular_input')
 
-        self.logger.info("Speedtest running")
+    def run(self, stanza, cleaned_params, input_config):
 
-    def handle_results(self, results, session_key, in_preview):
+        #interval = cleaned_params["interval"]
+        host = cleaned_params.get("host", None)
+        index = cleaned_params.get("index", "default")
+        sourcetype = cleaned_params.get("sourcetype", "speedtest")
 
-        # FYI: we ignore results since this is a generating command
+        server = cleaned_params.get("server", None)
+        runs = cleaned_params.get("runs", 3)
 
-        # Do the speedtest
-        index = get_default_index(session_key)
-
-        result = speedtest(host=self.server, runs=self.runs, index=index, logger=self.logger)
+        self.logger.info("Starting speedtest, stanza=%s", stanza)
+        result = speedtest(host=server, runs=runs, index=index, logger=self.logger)
 
         # Output the results
-        self.output_results([result])
+        self.output_event(result, stanza, index, sourcetype, stanza, host)
+
+        self.logger.info("Speedtest complete, stanza=%s", stanza)
 
 if __name__ == '__main__':
-    Speedtest.execute()
+
+    speedtest_input = None
+
+    try:
+        speedtest_input = SpeedtestInput()
+        speedtest_input.execute()
+        sys.exit(0)
+    except Exception as e:
+
+        # This logs general exceptions that would have been unhandled otherwise (such as coding
+        # errors)
+        if speedtest_input is not None and speedtest_input.logger is not None:
+            speedtest_input.logger.exception("Unhandled exception was caught, this may be due to a defect in the script")
+        else:
+            raise e
